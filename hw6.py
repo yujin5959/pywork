5f6304ba33f0c0cb2521a1b1ab37a8a29e152cee
지유진 <69490799+yujin5959@users.noreply.github.com>
2023-04-14 14:19:26 +0900

    hw6.py

---
+++ hw6.py
@@ -0,0 +1,11 @@
+for i in range(1,10,1):
+  for j in range(2,6,1):
+    print(f'{j} X {i} = {i * j:2d}',"\t",end=' ')
+  print('')
+
+print( )
+
+for i in range(1,10):
+    for j in range(6,10):
+      print(f'{j} X {i} = {i * j:2d}','\t',end=' ')
+    print(" ")
